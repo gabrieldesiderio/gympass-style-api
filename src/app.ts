2cc750a5ee7a70f7b0534bea6a0507e05da51ea6
Gabriel Desidério <gabrieldesd7@gmail.com>
2023-03-19 15:06:54 -0300

    Create docker-compose and setup prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Gabriel Desidério',
+    email: 'gabrieldesd7@gmail.com',
+  },
+})
